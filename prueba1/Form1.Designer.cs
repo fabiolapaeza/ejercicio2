879ed8a945ffd7be90b65b667bd8db8df8bee8bf
fabiolapaeza <fabiolapaeza@gmail.com>
2021-05-07 23:22:57 -0600

    Agregar archivos de proyecto.

---
+++ prueba1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace prueba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblApellidoP = new System.Windows.Forms.Label();
+            this.lblApelliodoM = new System.Windows.Forms.Label();
+            this.txtApellidoP = new System.Windows.Forms.TextBox();
+            this.txtApellidoM = new System.Windows.Forms.TextBox();
+            this.lblCalle = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblColonia = new System.Windows.Forms.Label();
+            this.txtCalle = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtColonia = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.dataTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(727, 241);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(110, 38);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(32, 92);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(67, 20);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(105, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(192, 27);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // lblApellidoP
+            // 
+            this.lblApellidoP.AutoSize = true;
+            this.lblApellidoP.Location = new System.Drawing.Point(303, 92);
+            this.lblApellidoP.Name = "lblApellidoP";
+            this.lblApellidoP.Size = new System.Drawing.Size(127, 20);
+            this.lblApellidoP.TabIndex = 5;
+            this.lblApellidoP.Text = "Apellido Paterno: ";
+            // 
+            // lblApelliodoM
+            // 
+            this.lblApelliodoM.AutoSize = true;
+            this.lblApelliodoM.Location = new System.Drawing.Point(643, 92);
+            this.lblApelliodoM.Name = "lblApelliodoM";
+            this.lblApelliodoM.Size = new System.Drawing.Size(129, 20);
+            this.lblApelliodoM.TabIndex = 6;
+            this.lblApelliodoM.Text = "Apellido materno:";
+            // 
+            // txtApellidoP
+            // 
+            this.txtApellidoP.Location = new System.Drawing.Point(436, 89);
+            this.txtApellidoP.Name = "txtApellidoP";
+            this.txtApellidoP.Size = new System.Drawing.Size(201, 27);
+            this.txtApellidoP.TabIndex = 7;
+            // 
+            // txtApellidoM
+            // 
+            this.txtApellidoM.Location = new System.Drawing.Point(778, 89);
+            this.txtApellidoM.Name = "txtApellidoM";
+            this.txtApellidoM.Size = new System.Drawing.Size(189, 27);
+            this.txtApellidoM.TabIndex = 8;
+            // 
+            // lblCalle
+            // 
+            this.lblCalle.AutoSize = true;
+            this.lblCalle.Location = new System.Drawing.Point(32, 161);
+            this.lblCalle.Name = "lblCalle";
+            this.lblCalle.Size = new System.Drawing.Size(45, 20);
+            this.lblCalle.TabIndex = 9;
+            this.lblCalle.Text = "Calle:";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(362, 164);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(66, 20);
+            this.lblNumero.TabIndex = 10;
+            this.lblNumero.Text = "Número:";
+            // 
+            // lblColonia
+            // 
+            this.lblColonia.AutoSize = true;
+            this.lblColonia.Location = new System.Drawing.Point(565, 164);
+            this.lblColonia.Name = "lblColonia";
+            this.lblColonia.Size = new System.Drawing.Size(63, 20);
+            this.lblColonia.TabIndex = 11;
+            this.lblColonia.Text = "Colonia:";
+            // 
+            // txtCalle
+            // 
+            this.txtCalle.Location = new System.Drawing.Point(84, 161);
+            this.txtCalle.Name = "txtCalle";
+            this.txtCalle.Size = new System.Drawing.Size(272, 27);
+            this.txtCalle.TabIndex = 12;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(434, 161);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(125, 27);
+            this.txtNumero.TabIndex = 13;
+            // 
+            // txtColonia
+            // 
+            this.txtColonia.Location = new System.Drawing.Point(634, 161);
+            this.txtColonia.Name = "txtColonia";
+            this.txtColonia.Size = new System.Drawing.Size(333, 27);
+            this.txtColonia.TabIndex = 14;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(857, 241);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(110, 38);
+            this.btnConsultar.TabIndex = 15;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // dataTable
+            // 
+            this.dataTable.AllowUserToAddRows = false;
+            this.dataTable.AllowUserToDeleteRows = false;
+            this.dataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTable.Location = new System.Drawing.Point(84, 275);
+            this.dataTable.Name = "dataTable";
+            this.dataTable.ReadOnly = true;
+            this.dataTable.RowHeadersWidth = 51;
+            this.dataTable.RowTemplate.Height = 29;
+            this.dataTable.Size = new System.Drawing.Size(582, 188);
+            this.dataTable.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 497);
+            this.Controls.Add(this.dataTable);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtColonia);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtCalle);
+            this.Controls.Add(this.lblColonia);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblCalle);
+            this.Controls.Add(this.txtApellidoM);
+            this.Controls.Add(this.txtApellidoP);
+            this.Controls.Add(this.lblApelliodoM);
+            this.Controls.Add(this.lblApellidoP);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnGuardar);
+            this.MinimumSize = new System.Drawing.Size(800, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Programa01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblApellidoP;
+        private System.Windows.Forms.Label lblApelliodoM;
+        private System.Windows.Forms.TextBox txtApellidoP;
+        private System.Windows.Forms.TextBox txtApellidoM;
+        private System.Windows.Forms.Label lblCalle;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblColonia;
+        private System.Windows.Forms.TextBox txtCalle;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtColonia;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.DataGridView dataTable;
+    }
+}
+
---
+++ prueba1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba1
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conexion = new MySqlConnection("Server=localhost; Database=prueba; Uid=root; Pwd=; Allow User Variables=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }   
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+                string query = "INSERT INTO persons (nombre, apellido_paterno, apellido_materno) " +
+                                "VALUES ('" + txtNombre.Text + "', '" + txtApellidoP.Text + "', '" + txtApellidoM.Text + "');" +
+                                "SET @lastidPerson = LAST_INSERT_ID();" +
+                                "INSERT INTO addresses (calle, numero, colonia) " +
+                                "VALUES ('" + txtCalle.Text + "', '" + txtNumero.Text + "', '" + txtColonia.Text + "');" +
+                                "SET @lastidAddress = LAST_INSERT_ID();" +
+                                "INSERT INTO distributors (id_distributor, fecha_registro, id_person, id_address)" +
+                                "VALUES('83JK', CURRENT_DATE, @lastidPerson, @lastidAddress)";
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                MessageBox.Show("Se ha guardado el distribuidor correctamente");
+            } catch(Exception)
+            {
+                MessageBox.Show("Ocurrió un errror");
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            MySqlCommand comando = new MySqlCommand("SELECT CONCAT(nombre, ' ', apellido_paterno, ' ', apellido_materno) AS 'Nombre Completo', " +
+                                                    "calle AS Calle, numero AS Número, colonia AS Colonia FROM distributors " +
+                                                    "INNER JOIN addresses on addresses.id_address = distributors.id_address " +
+                                                    "INNER JOIN persons on persons.id_person = distributors.id_person", conexion);
+            MySqlDataAdapter adaptador = new MySqlDataAdapter();
+            adaptador.SelectCommand = comando;
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            dataTable.DataSource = tabla;
+        }
+    }
+}
